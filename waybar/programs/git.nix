31ecfd6da5241214ffe7b8d1152934360f9302dc
fanzi03 <tirolrol15@gmail.com>
2025-11-15 11:55:33 +0800

    yep

--- waybar/programs/git.nix
+++
@@ -1,7 +0,0 @@
-{
-	programs.git = {
-		enable = true;
-		userName = "fanzi03";
-		userEmail = "tirolrol15@gmail.com";
-	};
-}
--- waybar/scripts/timers.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# timers: sleep + notify (very small, commented)
-sleep "${1:-25m}"                    # ждать аргумент или 1s
-notify-send "Таймер" "Готово — relax 5 min"  # показать уведомление через mako
-
